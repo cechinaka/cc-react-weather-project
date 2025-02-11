c396efbe3e5ea4c2289a7a7ef870f5216d15a741
cechinaka <chinaka08@gmail.com>
2025-02-10 21:23:24 -1000

    added footer and links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
     <div className="App">
       <div className='container'>
         <h1>Weather Application</h1>
-      
+        <footer>
+          This project was coded by <a href="https://github.com/cechinaka" target="_blank" rel="noreferrer">Christie Chinaka</a> and is on <a href="https://github.com/cechinaka/cc-react-weather-project" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://golden-liger-09cb10.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+        </footer>
       </div>
     </div>
   );
