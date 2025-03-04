354acdef3878cb33450ed17ee8093cd2d5390a67
cechinaka <chinaka08@gmail.com>
2025-03-04 11:05:46 -0500

    added search engine functionality

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import axios from "axios";
 export default function Weather(props) {
 
     const [weatherData, setWeatherData] = useState({ready: false});
+    const [city, setCity] = useState (props.defaultCity);
 
     function showWeather (response) {
         console.log(response);
@@ -22,14 +23,29 @@ export default function Weather(props) {
         });
     }
 
+    function search() {
+        const apiKey = `b8bt1eedc53a49e91cf7bb6aob435022`;
+        let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+        axios.get(url).then(showWeather);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
     if (weatherData.ready) {
 
     return (
         <div className="Weather">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city" className="searchBar w-100" autoFocus="on" />
+                        <input type="search" placeholder="Enter a city" className="searchBar w-100" autoFocus="on" onChange={updateCity} />
                     </div>
                     <div className="col-3">
                         <input type="submit" value="Search" className="btn btn-primary w-100" />
@@ -42,13 +58,11 @@ export default function Weather(props) {
     );
 } else {
     
-    const apiKey = `b8bt1eedc53a49e91cf7bb6aob435022`;
-    let url = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
-    axios.get(url).then(showWeather);
+    search();
     
     return (
     <div>
         <p>Loading...</p>
-        </div>);
+    </div>);
 }
 }
